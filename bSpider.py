11cc6b703ab73a8150503f4df768821438b3bcd9
Garin0828 <awGZbyXZEeJNnufCVKFoQg@hotmail.com>
2022-05-07 19:48:16 +0800

    asd

--- bSpider.py
+++ bSpider.py
@@ -3,6 +3,7 @@ from urllib.error import *
 import re,json
 import time
 import os,sys
+import http.client
 
 header = {
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
@@ -58,15 +59,12 @@ def download_video(path,url,depth=0):
         d = urlopen(r).read()
         with open(path,"wb") as f:
             f.write(d)
-<<<<<<< HEAD
     except Exception :
         download_video(path,url,depth+1)
-        
-=======
-    except HTTPError :
-        download_video(path,url)
+    except http.client.IncompleteRead :
+        download_video(path,url,depth+1)
     
->>>>>>> 8796d5ec34ea1bb2a98f44138ff3fa3b651da912
+
 if __name__=="__main__":
     print(sys.argv)
     #t1,a=getHighVideoURL("BV1Nr4y1n751")
